e15667f2bb18b46915e212cfaba83cb802bf4a5e
David Andersson <lejonmanen@gmail.com>
2024-08-21 11:33:57 +0200

    Övning 2.1

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,23 @@ import './style.css'
 // Plocka ut element ur DOM
 // State-variabel för antal klick
 // Event listener för button, som uppdaterar innerText i <p>
+type Callback = (event: Event) => void;
 
+const countButton: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>('.count-button')
+const infoPara: HTMLParagraphElement | null = document.querySelector<HTMLParagraphElement>('.info')
+// Alternativ: lägg till "!" eller "as HTMLButtonElement"
+
+let clicks: number = 0
+if( countButton && infoPara ) {
+	const whenClick: Callback = () => {  // behöver inte event vid klick-händelser
+		// öka state-variabeln
+		// uppdatera texten i infoPara
+		clicks++
+		infoPara.innerText = `Du har klickat ${clicks} gånger!`
+	}
+
+	countButton.addEventListener('click', whenClick)
+	// Alternativ till if-sats: countButton?.addEventListener.....
+} else {
+	console.log('Oh noes vi hittar inte DOM-elementen!')
+}
