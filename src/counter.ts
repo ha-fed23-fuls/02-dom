5fab3e7b2531b7007c3ea09850c461417d093d01
David Andersson <lejonmanen@gmail.com>
2024-08-21 10:29:57 +0200

    Intro till övning 2.1

--- src/counter.ts
+++ src/counter.ts
@@ -1,5 +1,5 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter: number = 0
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// 1 Skapa en webbsida med ett button- och ett p-element. När man klickar på knappen ska texten i p-elementet uppdateras, så att den visar hur många gånger man har klickat på knappen:
+// <p class="info"> Du har klickat 25 gånger. </p>
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Plocka ut element ur DOM
+// State-variabel för antal klick
+// Event listener för button, som uppdaterar innerText i <p>
+
